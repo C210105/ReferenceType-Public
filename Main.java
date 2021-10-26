eea1ca1aef663a310a254ba6c7f4cb748b56ccdd
DANGLONG <c210105@ccg.ac.jp>
2021-10-26 10:28:16 +0900

    問題６

--- Main.java
+++ Main.java
@@ -5,16 +5,17 @@ public class Main {
         String obj2 = "B";
         String obj3 = "C";
 
-        String answer = obj2;
+        String answer = new String("B");
 
         System.out.println("obj1: " + obj1);
         System.out.println("obj2: " + obj2);
         System.out.println("obj3: " + obj3);
 
         System.out.println("----------");
-        System.out.println("answer: " + answer);
-        if(obj2 == answer) {
-            System.out.println("obj2とanswerは同じ参照値です。");
+        // System.out.println("answer: " + answer);
+        if(answer.equals(obj2)) {
+            // System.out.println("obj2とanswerは同じ参照値です。");
+            System.out.println("answer: " + answer);
         }
     }
 
